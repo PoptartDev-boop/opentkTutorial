31f91e445b9834321b58222747f1614c2adaeac6
PoptartDev <73976765+PoptartDev-boop@users.noreply.github.com>
2021-07-13 13:54:07 -0700

    Add project files.

---
+++ opentkTutorial/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Opentk namespaces
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace opentkTutorial
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState key = Keyboard.GetState();
+
+            if (key.IsKeyDown(Key.Escape))
+                Exit();
+        }
+    }
+}
